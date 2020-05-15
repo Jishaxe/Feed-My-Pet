ac824c1e140cdb46f0b4fcb6245b16a74be4f5a9
Josh Lee <josh@joshbe.me>
2020-05-15 17:20:26 +0100

    untrack some stuff we dont neeed

--- Feed My Pet/Assets/StreamingAssets/TwitchBot/index.js
+++ Feed My Pet/Assets/StreamingAssets/TwitchBot/index.js
@@ -1,20 +1,17 @@
 const TwitchBot = require('twitch-bot')
- 
-const Bot = new TwitchBot({
-  username: 'petcarebot',
-  oauth: 'oauth:keunzlmrelfh2sw0b023jdwef3sq34',
-  channels: ['jshxe']
-})
- 
-Bot.on('join', channel => {
+
+var join = (channel) => {
   console.log(`Joined channel: ${channel}`)
-})
+}
  
-Bot.on('error', err => {
-  console.log(err)
-})
- 
-Bot.on('message', chatter => {
+var error = (err) => {
+  if (err.code == "ECONNRESET") {
+    console.log("Disconnected, reconnecting...")
+    connect()
+  }
+}
+
+var message = (chatter) => {
   if(chatter.message.toLowerCase().startsWith("!feed")) {
       if (chatter.message.split(" ").length != 2) {
         console.log("CMD feed 1")
@@ -28,5 +25,20 @@ Bot.on('message', chatter => {
 
       console.log("CMD feed " + num);
   }
-})
+}
 
+var Bot = null
+
+var connect = () => {
+  Bot = new TwitchBot({
+    username: 'petcarebot',
+    oauth: 'oauth:keunzlmrelfh2sw0b023jdwef3sq34',
+    channels: ['jshxe']
+  })  
+
+  Bot.on("message", message)
+  Bot.on('error', error)
+  Bot.on('join', join)
+}
+
+connect()
